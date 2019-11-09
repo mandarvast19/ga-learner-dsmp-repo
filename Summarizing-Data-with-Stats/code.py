a49ee5f05890d29c086ce118ed82bef25dfa3c2e
mandarvast19 <mandarvast19@users.noreply.github.com>
2019-11-09 15:44:29 +0530

    Code file created.

---
+++ Summarizing-Data-with-Stats/code.py
@@ -0,0 +1,55 @@
+# --------------
+#Header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#path of the data file- path
+
+#Code starts here 
+data = pd.read_csv(path)
+data['Gender'].replace('-',value='Agender',inplace=True)
+gender_count = data['Gender'].value_counts()
+gender_count.plot(kind = "bar")
+
+
+
+# --------------
+#Code starts here
+alignment = data['Alignment'].value_counts()
+plt.pie(alignment,labels=['good','bad','neutral'])
+
+
+# --------------
+#Code starts here
+sc_df = data[['Strength','Combat']]
+sc_covariance = sc_df.cov().iloc[0,1]
+sc_strength = sc_df['Strength'].std()
+sc_combat = sc_df['Combat'].std()
+sc_pearson = sc_covariance/(sc_strength*sc_combat)
+ic_df = data[['Intelligence','Combat']]
+ic_covariance = ic_df.cov().iloc[0,1]
+ic_intelligence = ic_df['Intelligence'].std()
+ic_combat = ic_df['Combat'].std()
+ic_pearson = ic_covariance/(ic_intelligence*ic_combat)
+print(sc_pearson)
+print(ic_pearson)
+
+
+# --------------
+#Code starts here
+total_high = data['Total'].quantile(q=0.99)
+super_best = data[data['Total']>total_high]
+super_best_names = list(super_best['Name'])
+print(super_best_names)
+
+
+# --------------
+#Code starts here
+fig,(ax_1,ax_2,ax_3) = plt.subplots(1,3,figsize=(20,5))
+data.boxplot('Intelligence',ax=ax_1)
+data.boxplot('Speed',ax=ax_2)
+data.boxplot('Power',ax=ax_3)
+
+
